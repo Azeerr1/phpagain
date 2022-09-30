50e50992209147e5d69cb9764297f3c7ac46434d
Azer <96022430+Azeerr1@users.noreply.github.com>
2022-09-30 16:01:52 +0200

    Add files via upload

--- verification.php
+++ verification.php
@@ -1,23 +1,24 @@
 <?php
 session_start();
-if(isset($_POST['prenom']) && isset($_POST['motdepasse']))
+if(isset($_POST['username']) && isset($_POST['password']))
 {
     // connexion à la base de données
-    $db_username = 'root';
-    $db_password = '';
+    $db_username = 'test';
+    $db_password = 'mdp123';
     $db_name     = 'inscription';
     $db_host     = 'localhost';
     $db = mysqli_connect($db_host, $db_username, $db_password,$db_name)
-           or die('Erreur: Ne peut pas se connecter à la base de données.');
+           or die('could not connect to database');
 
     // on applique les deux fonctions mysqli_real_escape_string et htmlspecialchars
     // pour éliminer toute attaque de type injection SQL et XSS
-    $username = mysqli_real_escape_string($db,htmlspecialchars($_POST['prenom']));
-    $password = mysqli_real_escape_string($db,htmlspecialchars($_POST['motdepasse']));
+    $username = mysqli_real_escape_string($db,htmlspecialchars($_POST['username']));
+    $password = mysqli_real_escape_string($db,htmlspecialchars($_POST['password']));
 
     if($username !== "" && $password !== "")
     {
-        $requete = "SELECT count(*) FROM utilisateur where prenom = '".$username."' and motdepasse = '".$password."' ";
+        $requete = "SELECT count(*) FROM information where
+              prenom = '".$username."' and motdepasse = '".$password."' ";
         $exec_requete = mysqli_query($db,$requete);
         $reponse      = mysqli_fetch_array($exec_requete);
         $count = $reponse['count(*)'];
